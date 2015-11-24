0f4c0f006f9c3f72c203fecbe975c2a02ae94439
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-11-24 10:28:38 +0200

    add lab6

---
+++ lab6/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Windows.Media.Media3D;
+
+namespace lab6
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private Point3D _position;
+        private double _x, _y;
+
+        public Point3D Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                OnPropertyChanged(nameof(Position));
+            }
+        }
+
+        public double X
+        {
+            get { return _x; }
+            set
+            {
+                _x = value;
+                OnPropertyChanged(nameof(X));
+            }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+            set
+            {
+                _y = value;
+                OnPropertyChanged(nameof(Y));
+            }
+        }
+    }
+}
